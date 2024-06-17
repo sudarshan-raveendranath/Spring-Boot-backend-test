a18da7465379ddf02cef95fc640feebf268db284
sudarshan raveendranath dayarathna <120754357+sudarshan-raveendranath@users.noreply.github.com>
2024-06-17 16:52:02 +0530

    student controller

--- src/main/java/org/example/test2/application/controllers/StudentController.java
+++ src/main/java/org/example/test2/application/controllers/StudentController.java
@@ -1,4 +1,15 @@
 package org.example.test2.application.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "api/v1/student")
+@RestController
 public class StudentController {
+
+    @GetMapping("/getStudent")
+    public String getStudent() {
+        return "Student";
+    }
 }
